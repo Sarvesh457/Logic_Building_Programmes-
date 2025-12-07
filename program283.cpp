d8a7088a0b46ca2c928ba101e59877c6f911dcc5
Sarvesh Mahajan <sarveshmahajan457@gmail.com>
2025-12-07 20:01:38 +0530

    Add files via upload

---
+++ program283.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+void Dispaly()
+{
+    int iCnt = 0;
+
+    iCnt = 1;
+    while (iCnt <= 4)
+    {    
+    cout<<"Jay Ganesh...\n";
+    iCnt++;
+    }
+    
+}
+int main()
+{
+    Dispaly();
+
+return 0;
+}
