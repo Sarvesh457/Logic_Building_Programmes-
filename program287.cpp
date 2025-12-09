67ee01500fdc3831c61aa8d1eb3f49fd648e8e34
Sarvesh Mahajan <sarveshmahajan457@gmail.com>
2025-12-09 15:22:38 +0530

    Add files via upload

---
+++ program287.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+void Display()
+{
+    static int iCnt = 0;               //Static storage class     change
+
+    cout<<"Jay Ganesh..."<<iCnt<<"\n";
+    iCnt++;
+    Display();
+    
+}
+int main()
+{
+    Display();
+
+return 0;
+}
