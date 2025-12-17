3050a0d00eb94b84af7218926bd0d8b36cffed4b
Sarvesh Mahajan <sarveshmahajan457@gmail.com>
2025-12-17 22:34:17 +0530

    Add files via upload

---
+++ program296.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class program296
+{
+    public static void main(String A[])
+    {
+        Vector <String> vobj = new Vector <String> ();            // generic    
+
+        vobj.add("c");
+        vobj.add("c++");
+        vobj.add("Java");
+        vobj.add("Python");
+        vobj.add("C#");
+        vobj.add("Java");
+
+
+        System.out.println(vobj);
+
+        vobj.add(2,"LSP");         // to add  at index
+
+        System.out.println(vobj);
+
+        System.out.println(vobj.capacity());            //show capacity
+
+        System.out.println(vobj.contains("LSP"));         // is there or not in boolean true or fla
+
+        System.out.println(vobj.contains("Linux")); 
+
+        vobj.remove(6);                             //remove
+
+        System.out.println(vobj);
+
+    }
+}
