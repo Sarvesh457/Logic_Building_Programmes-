9363dd2d3a32ede851170a56570a7bf15964d50b
Sarvesh Mahajan <sarveshmahajan457@gmail.com>
2026-01-10 23:29:39 +0530

    Add files via upload

---
+++ program343.js
@@ -0,0 +1,23 @@
+// Accept and display 
+function Factorial(no)
+{
+    let iFact = 1;
+
+    while(no !=0)
+    {
+        iFact = iFact * no;
+        no--;
+    }
+    return iFact;
+}
+function main()
+{
+    let iValue = 10,iRet = 0;
+
+    iRet= Factorial(iValue);
+
+    console.log("Factroial of ${iValue} is ${iRet}");
+
+}
+
+main();
