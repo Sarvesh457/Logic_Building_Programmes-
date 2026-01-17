4b2317f80904577e7b262338f062cb1d2c6d1d83
Sarvesh Mahajan <sarveshmahajan457@gmail.com>
2026-01-17 23:28:47 +0530

    Add files via upload

---
+++ program365.js
@@ -0,0 +1,12 @@
+function main()
+{
+    let obj = {name : "Sarvesh",salary : 2100000};              //JSON
+
+    //console.log(obj[0]);              Not Allowed
+
+    console.log(obj.name);
+    console.log(obj.salary);
+
+    //console.log(obj[name]);               Not Allowed
+}
+main();
